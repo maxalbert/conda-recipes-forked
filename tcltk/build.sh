d5ff683c939151a8a70bd202e18fca1910c5eeb6
Maximilian Albert <maximilian.albert@gmail.com>
2015-09-26 12:06:53 +0000

    Upgrade tcltk recipe to version 8.6.4.

--- tcltk/build.sh
+++ tcltk/build.sh
@@ -2,19 +2,19 @@
 
 VER=$PKG_VERSION
 
-curl "http://superb-dca2.dl.sourceforge.net/project/tcl/Tcl/${VER}/tcl${VER}-src.tar.gz" > tcl${VER}-src.tar.gz
-curl "http://superb-dca2.dl.sourceforge.net/project/tcl/Tcl/${VER}/tk${VER}-src.tar.gz" > tk${VER}-src.tar.gz
+wget http://downloads.sourceforge.net/project/tcl/Tcl/${VER}/tcl${VER}-src.tar.gz
+wget http://downloads.sourceforge.net/project/tcl/Tcl/${VER}/tk${VER}-src.tar.gz
 
 tar xzf tcl${VER}-src.tar.gz
 tar xzf tk${VER}-src.tar.gz
 
 cd $SRC_DIR/tcl${VER}/unix
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX --enable-threads --enable-64bit
 make
 make install
 
 cd $SRC_DIR/tk${VER}/unix
-./configure --with-tcl=$PREFIX/lib --prefix=$PREFIX --enable-aqua=yes
+./configure --with-tcl=$PREFIX/lib --prefix=$PREFIX --enable-aqua=yes --enable-threads --enable-64bit
 make
 make install
 
