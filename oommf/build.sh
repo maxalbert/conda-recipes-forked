8453bc24273e960414b3ae8befc0b46371bb8046
Maximilian Albert <maximilian.albert@gmail.com>
2015-09-27 13:24:23 +0000

    Add recipe for OOMMF (version 12a5bis_20120928).

---
+++ oommf/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+TCLTKVERSION=8.6
+
+# Compile OOMMF
+export OOMMF_TCL_CONFIG=/home/vagrant/miniconda/envs/phd/lib/tclConfig.sh
+export OOMMF_TK_CONFIG=/home/vagrant/miniconda/envs/phd/lib/tkConfig.sh
+tclsh${TCLTKVERSION} oommf.tcl pimake distclean
+tclsh${TCLTKVERSION} oommf.tcl pimake upgrade
+tclsh${TCLTKVERSION} oommf.tcl pimake
+tclsh${TCLTKVERSION} oommf.tcl +platform
+
+# Copy all OOMMF sources and compiled files into $PREFIX/opt/
+install -d ${PREFIX}/opt/
+install -d ${PREFIX}/bin/
+cp -r ${SRC_DIR}/* ${PREFIX}/opt/
+
+# Create an executable called 'oommf' in ${PREFIX}/bin which
+# calls the OOMMF executable in $PREFIX/opt/
+oommf_command=$(cat <<EOF
+#! /bin/bash
+tclsh${TCLTKVERSION} $PREFIX/opt/oommf.tcl "\$@"
+EOF
+)
+echo "$oommf_command" > ${PREFIX}/bin/oommf
+chmod a+x ${PREFIX}/bin/oommf
