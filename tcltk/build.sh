c7279a00e38402e2046547d6d293cd2909ab386a
Maximilian Albert <maximilian.albert@gmail.com>
2015-10-19 18:30:10 +0100

    Add link 'bin/tclsh' which points to bin/tclsh8.6

--- tcltk/build.sh
+++ tcltk/build.sh
@@ -13,12 +13,14 @@ wget http://downloads.sourceforge.net/project/tcl/Tcl/${VER}/tk${VER}-src.tar.gz
 tar xzf tcl${VER}-src.tar.gz
 tar xzf tk${VER}-src.tar.gz
 
+# Compile and install tcl
 cd $SRC_DIR/tcl${VER}/unix
 ./configure --prefix=$PREFIX --enable-threads --enable-64bit
 make
 make install
 make install-private-headers
 
+# Compile and install tk
 cd $SRC_DIR/tk${VER}/unix
 ./configure --with-tcl=$PREFIX/lib --prefix=$PREFIX --enable-aqua=yes --enable-threads --enable-64bit
 make
@@ -35,3 +37,6 @@ sed -i "s|^TK_SRC_DIR=.*$|TK_SRC_DIR='$PREFIX/src/tk${VER}'|" $PREFIX/lib/tkConf
 
 cd $PREFIX
 rm -rf man share
+
+cd $PREFIX/bin
+ln -s tclsh8.6 tclsh
